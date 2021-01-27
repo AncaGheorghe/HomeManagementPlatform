141f7af756b4a8f7f9fdfe91db2bc26b7b7c598e
Anca <anca.gheorghe1996@gmail.com>
2021-01-27 21:20:27 +0200

    third commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,16 @@ import {HomePageComponent} from './home-page/home-page.component';
 import {AddTaskComponent} from './add-task/add-task.component';
 import {AddGroupComponent} from './add-group/add-group.component';
 import {MyGroupsComponent} from './my-groups/my-groups.component';
+import {AuthGuardGuard} from './guards/auth-guard.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomePageComponent },
-  { path: 'addTask', component: AddTaskComponent,},
-  { path: 'addGroup', component: AddGroupComponent,},
-  { path: 'myGroups', component: MyGroupsComponent,}
+  { path: 'home', component: HomePageComponent, canActivate: [AuthGuardGuard]},
+  { path: 'addTask', component: AddTaskComponent, canActivate: [AuthGuardGuard]},
+  { path: 'addGroup', component: AddGroupComponent,canActivate: [AuthGuardGuard]},
+  { path: 'myGroups', component: MyGroupsComponent,canActivate: [AuthGuardGuard]}
 ];
 
 @NgModule({
