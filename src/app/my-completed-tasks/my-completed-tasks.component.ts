a3922a7f213c0c72e33f5f21c47e52d253c707df
corina <costincorina89@yahoo.com>
2021-01-30 16:53:38 +0200

    my-opened-tasks + my-completed-tasks(html + css)

--- src/app/my-completed-tasks/my-completed-tasks.component.ts
+++ src/app/my-completed-tasks/my-completed-tasks.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+declare function require(path: string);
 @Component({
   selector: 'app-my-completed-tasks',
   templateUrl: './my-completed-tasks.component.html',
   styleUrls: ['./my-completed-tasks.component.css']
 })
 export class MyCompletedTasksComponent implements OnInit {
+
+  circleImage = require('src/assets/img/circle.svg').default;
 
   constructor() { }
 
